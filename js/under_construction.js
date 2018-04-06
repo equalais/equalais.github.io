73ef3201f9e76d46ce20c930982e5892d66413d2
Thom Miano <tmiano@rti.org>
2018-04-06 03:12:33 -0400

    put a date on the page

--- js/under_construction.js
+++ js/under_construction.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("April 12, 2018 12:00:00").getTime();
+var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
